3cb40e383fd6f05350e08f2683824d78a0d15b42
c-lodi <iclaude1601@gmail.com>
2024-05-16 14:17:03 +0400

    tokens-page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect, useRef } from 'react';
 import Header from './components/Header';
 import './App.css';
 import Swap from './components/Swap';
@@ -7,31 +8,49 @@ import Footer from './components/Footer';
 import { Routes, Route } from "react-router-dom";
 import { useConnect, useAccount } from "wagmi";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-
+import axios from 'axios';
 
 function App() {
 
+  const tokensHasFetched = useRef(false)
+
+  const [tokens, setTokens] = useState([]);
+
+  async function getTokenLists() {
+    const url = "http://localhost:3001/tokenList";
+    const response = await axios.get(url);
+    const tokenArray = Object.values(response.data);
+    setTokens(tokenArray)
+  }
+
+  useEffect(() => {
+    if (!tokensHasFetched.current) {
+      getTokenLists()
+      tokensHasFetched.current = true;
+    }
+  }, [])
+
+
+
   const { address, isConnected } = useAccount();
-  const { connect } = useConnect ({
+  const { connect } = useConnect({
     connector: new MetaMaskConnector(),
-  }); 
+  });
 
   return (
     <div className="App">
 
       <Header connect={connect} isConnected={isConnected} address={address} />
       <Routes>
-          <Route path="/" element={<Swap isConnected={isConnected} address={address} />} />
-          <Route path="/tokens" element={<Tokens />} />
-          <Route path="/liquidity" element={<Liquidity />} />
+        <Route path="/" element={<Swap isConnected={isConnected} address={address} tokens={tokens} />} />
+        <Route path="/tokens" element={<Tokens tokens={tokens} />} />
+        <Route path="/liquidity" element={<Liquidity />} />
+
+      </Routes>
+      <Footer />
+      </div>
 
 
-        </Routes>
-        <Footer />
-       
-       
-  
-    </div>
   );
 }
 
--- src/components/Tokens.js
+++ src/components/Tokens.js
@@ -1,9 +1,58 @@
 import React from "react";
+import { Table } from "flowbite-react";
+import ETH from "../assets/eth.svg"
 
-function Tokens() {
+function Tokens(tokenProps) {
 
-    return(
-        <div>OLANESS</div>
+    const tokens = tokenProps.tokens
+
+
+    function truncateString(str, num) {
+        if (str.length > num) {
+            return str.slice(0, num) + '...';
+        } else {
+            return str;
+        }
+    }
+
+    return (
+        <div className="inset-0 flex items-center justify-center bg-gun-metal-900 min-h-screen py-[200px]">
+            <div className="max-w-full overflow-auto">
+
+                <Table hoverable className="min-w-[600px]">
+                    <Table.Head>
+                        <Table.HeadCell className="bg-gun-metal-800 blue-gray">Coin Name</Table.HeadCell>
+                        <Table.HeadCell className="bg-gun-metal-800 blue-gray">Symbol</Table.HeadCell>
+                        <Table.HeadCell className="bg-gun-metal-800 blue-gray">Decimals</Table.HeadCell>
+                        <Table.HeadCell className="bg-gun-metal-800 blue-gray">Address</Table.HeadCell>
+                        <Table.HeadCell className="bg-gun-metal-800">
+                            <span className="sr-only">Trade</span>
+                        </Table.HeadCell>
+                    </Table.Head>
+                    <Table.Body className="divide-y">
+                        {tokens.map((token) => (
+                            <Table.Row key={token.address} className="bg-gun-metal-700 blue-gray border-slate-700 dark:bg-gray-800 hover:bg-slate-600">
+                                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                    <div className="flex items-center blue-gray">
+                                        <img src={token.logoURI} className="w-6 h-6 mr-2" alt={`${token.name} logo`} />
+                                        <span>{truncateString(token.name, 20)}</span>
+                                    </div>
+                                </Table.Cell>
+                                <Table.Cell>{truncateString(token.symbol, 5)} </Table.Cell>
+                                <Table.Cell>{token.decimals}</Table.Cell>
+                                <Table.Cell>{token.address.slice(0, 6) + "..." + token.address.slice(-4)}</Table.Cell>
+                                <Table.Cell>
+                                    <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                                        Trade
+                                    </a>
+                                </Table.Cell>
+                            </Table.Row>
+                        ))}
+                    </Table.Body>
+                </Table>
+            </div>
+        </div>
+
     )
 }
 
